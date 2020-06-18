929716b4dc3b5bc2b0294b07d3e665b2505d34c8
Akash Tiwari <54893915+akashtiwari370@users.noreply.github.com>
2020-06-18 19:51:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 print `ifconfig`;
 
 
+
+
+?>
+
 img src​="<?php echo getenv('url') ?>/1.jpg">
 img src​="<?php echo getenv('url') ?>/2.jpg">
 img src​="<?php echo getenv('url') ?>/3.jpg">
-
-?>
 
 </pre>
